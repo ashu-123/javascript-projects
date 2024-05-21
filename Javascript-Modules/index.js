50f66825188f1142c5ff7fd632a7b6ad7dc7f09d
Ashutosh <Ashutosh>
2024-05-21 09:40:10 +0530

    Modularizing javascript code

--- Javascript-Modules/index.js
+++ Javascript-Modules/index.js
@@ -1,31 +1,8 @@
 // Rounds to two digits
-function roundMoney(value) {
-  return Math.round(value * 100) / 100; // round to 2 places
-}
+import { roundMoney } from "./roundMoney.js";
 
 // Tax Rates
-const lowRate = .05;
-const highRate = .095;
-
-// Calculates taxes based on price
-function calcuateTax(totalPrice) {
-  let taxRate = 0;
-  
-  if (totalPrice >= 10 && totalPrice < 100) {
-    taxRate = lowRate;
-  } else {
-    taxRate = highRate;
-  }
-
-  const preTax = roundMoney(totalPrice);
-  const postTax = roundMoney((preTax * taxRate) + preTax);
-
-  return {
-    preTax,
-    postTax,
-    taxRate
-  };
-}
+import { calcuateTax } from "./calculateTax.js";
 
 // Create ten examples and use the 
 // calc function
